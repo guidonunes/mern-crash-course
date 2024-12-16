31b2582c91f6d967927835ababca41e11e59ca48
Guilherme Nunes <guilherme.augd@gmail.com>
2024-12-16 14:59:39 -0300

    import module

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 // const express = require('express');
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.get('/products', (req, res) => {
   res.send('API is running...');
-})
+});
+
+console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
   console.log('Server started on http://localhost:5000 ');
