42749d13041b15b73ac82034ebf3c6e09710a78e
Guilherme Nunes <guilherme.augd@gmail.com>
2024-12-16 14:36:41 -0300

    installed nodemon package and set up initial route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+})
+
 app.listen(5000, () => {
-  console.log('Server started on http://localhost:5000');
+  console.log('Server started on http://localhost:5000 ');
 })
