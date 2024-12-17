8d8ca1f235eb04601425b3696dbd8df3528ffa98
Guilherme Nunes <guilherme.augd@gmail.com>
2024-12-17 17:57:29 -0300

    implement logic of the put method

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,22 @@ app.post('/api/products', async (req, res) => {
   }
 });
 
+app.put('/api/products/:id', async (req, res) => {
+  const {id} = req.params;
+  const product = req.body;
+
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send('No product with that id');
+   }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+    res.status(200).json({success: true, data: updatedProduct});
+  } catch (error) {
+    res.status(500).json({success: false, message: 'Server Error'});
+  }
+});
+
 app.delete('/api/products/:id', async (req, res) => {
   const {id} = req.params;
 
