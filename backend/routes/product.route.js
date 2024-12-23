f99929904da5168eb0ad1d38ce7e9c588674addd
Guilherme Nunes <guilherme.augd@gmail.com>
2024-12-23 10:06:59 -0300

    create directory for the controller and moved get request method

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -4,15 +4,7 @@ import Product from '../models/product.model.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try{
-    const products = await Product.find({});
-    res.status(200).json({success: true, data: products});
-  }catch(error){
-    console.log("Error in get products", error.message);
-    res.status(500).json({success: false, message: 'Server Error'});
-  }
-});
+router.get('/', );
 
 router.post('/', async (req, res) => {
   const product = req.body;
