3d482cc834bff30ab8fa275b313112c5dfdd5c5e
Guilherme Nunes <guilherme.augd@gmail.com>
2024-12-23 15:27:23 -0300

    installed prime react dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { PrimeReactProvider } from 'primereact/api';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-   <ChakraProvider>
-    <App />
-   </ChakraProvider>
-  </StrictMode>,
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+
 )
