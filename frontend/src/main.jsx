f7d0917300110a5193e3fa878ed27713d0a5f918
Guilherme Nunes <guilherme.augd@gmail.com>
2024-12-23 10:58:48 -0300

    set up chakra provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+   <ChakraProvider>
     <App />
+   </ChakraProvider>
   </StrictMode>,
 )
